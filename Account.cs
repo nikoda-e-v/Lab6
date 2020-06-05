48f715fbf43517282fe495fe8ad7b94fb47d6116
Женя Никода <D:\Users\Nikoda\AppData\Roaming\The Bat!>
2020-06-05 07:35:17 +0300

    Init lab6

---
+++ Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab6
+{
+    public class Account
+    {
+        public int money { get; set; }
+        public bool isBlocked = false;
+    }
+}
---
+++ Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+   public class Bank
+    {
+        public void Transfer(Account from, Account to, int ammount)
+        {
+           if(!from.isBlocked && !to.isBlocked)
+            {
+                from.isBlocked = true;
+                to.isBlocked = true;
+                if (from.money - ammount >= 0)
+                {
+                    from.money -= ammount;
+                    to.money += ammount;
+                }
+                from.isBlocked = false;
+                to.isBlocked = false;
+            }
+            
+        }
+
+        public async void doTransfer(Account from, Account to, int ammount)
+        {
+            await Task.Run(() => Transfer(from, to, ammount));
+        }
+    }
+}
